6a468b3ce5b7fc21050845396347e994f5bd73f3
rakeshsivagouni <rakesh.sivagouni@gmail.com>
2024-09-16 17:59:55 +0000

    sep 16 code

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from RAKESH!";
+        return "updated information from rakesh Goud!";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Thankyou! Response received AWS Ecs";
+        return "Thankyou! Messages delivered";
     }
 }
