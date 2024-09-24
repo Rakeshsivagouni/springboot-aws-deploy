ea8ece7f4313eebe5c244a330f5354af36066c2b
rakeshsivagouni <rakesh.sivagouni@gmail.com>
2024-09-24 10:13:55 +0000

    final update

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Thankyou for the information";
+        return "Thankyou for the information!";
     }
 }
