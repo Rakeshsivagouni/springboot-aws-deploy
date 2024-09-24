8db765f1090a276c0896ca941197b4f67dd9d6ab
rakeshsivagouni <rakesh.sivagouni@gmail.com>
2024-09-24 09:39:02 +0000

    index.js file modified

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "second version of code from rakesh Goud on 17th september";
+        return "second version of code from rakesh Goud released on  24th of september";
     }
 
     @GetMapping("/message")
